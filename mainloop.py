9b95d02603b33303b8933b57d3459492de1c1d6c
Meredith Alley <malley@olin.edu>
2022-05-09 07:02:05 -0400

    Reformatting variables

--- mainloop.py
+++ mainloop.py
@@ -8,8 +8,10 @@ from game_assets import title_screen
 from game_assets import end_screen
 from game_assets import screen
 
-
+#Starts with the title screen
 title_screen(screen)
+
+#Loops between game and end/retry screen
 while True:
     final_score = play_level(screen)
     end_screen(screen, final_score)
