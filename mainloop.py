c097753ae0cefe73b74c50532a95df8346b7fcec
Meredith Alley <malley@olin.edu>
2022-05-09 06:11:08 -0400

    Formatting changes

--- mainloop.py
+++ mainloop.py
@@ -3,7 +3,6 @@ run the game from this file
 """
 
 # importing needed libraries
-import sys
 from game_assets import play_level
 from game_assets import title_screen
 from game_assets import end_screen
